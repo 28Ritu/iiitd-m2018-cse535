6cabbb52d693f176f6275df6e02b3f28787f4692
Ritu <ritu16078@iiitd.ac.in>
2018-09-03 22:38:20 +0530

    Initial Commit

--- A2_2016078/app/src/main/java/com/example/ritu/a2_2016078/SongMap_A2_2016078.java
+++ A2_2016078/app/src/main/java/com/example/ritu/a2_2016078/SongMap_A2_2016078.java
@@ -10,9 +10,6 @@ public class SongMap_A2_2016078 {
         hashMap = new HashMap<String, String>();
         hashMap.put("Boyfriend-Justin Bieber", "android.resource://com.example.ritu.a2_2016078/" + R.raw.boyfriend);
         hashMap.put("Kabhi To Pass Mere Aao - Atif", "android.resource://com.example.ritu.a2_2016078/" + R.raw.kabhitohpaasmereaao);
-        hashMap.put("Nit Khair Manga", "android.resource://com.example.ritu.a2_2016078/" + R.raw.nitkhairmangaraid);
-        hashMap.put("Sanu Ek Pal Chain", "android.resource://com.example.ritu.a2_2016078/" + R.raw.sanuekpalchain);
-        hashMap.put("Tere Liye", "android.resource://com.example.ritu.a2_2016078/" + R.raw.tereliye);
     }
 
     public HashMap<String, String> getHashMap() {
@@ -22,9 +19,6 @@ public class SongMap_A2_2016078 {
         ArrayList<String> arrayList = new ArrayList<>();
         arrayList.add("Boyfriend-Justin Bieber");
         arrayList.add("Kabhi To Pass Mere Aao - Atif");
-        arrayList.add("Nit Khair Manga");
-        arrayList.add("Sanu Ek Pal Chain");
-        arrayList.add("Tere Liye");
         return arrayList;
     }
 }
